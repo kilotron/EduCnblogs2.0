b457e2b973eeb181ac49f4f7137088a7906e84f2
Welkin1027 <38560474+Welkin1027@users.noreply.github.com>
2019-04-16 19:26:06 +0800

    voteDetail

--- CnBlogAndroid/Source/screens/VoteDetail.js
+++ CnBlogAndroid/Source/screens/VoteDetail.js
@@ -1,6 +1,6 @@
 import Config from '../config';
 import api from '../api/api.js';
-import {authData,err_info,StorageKey} from '../config'
+import { authData, err_info, StorageKey } from '../config'
 import * as Service from '../request/request.js'
 import MyAdapter from './MyAdapter.js';
 import React, { Component } from 'react';
@@ -24,58 +24,70 @@ import {
 import { Icon, Fab } from 'native-base';
 import ShareButton from './Share';
 const { height, width } = Dimensions.get('window');
-const ContentHandler = require('../DataHandler/BlogDetail/ContentHandler');
+const ContentHandler = require('../DataHandler/VoteDetail/ContentHandler');
 
-// 传入博客Id和blogApp和CommentCount作为参数
-export default class BlogDetail extends Component{
-    constructor(props){
+// 传入voteID作为参数
+export default class VoteDetail extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            content: '',
-            isRequestSuccess: false,
-            url:this.props.navigation.state.params.Url,
+            voteId:	this.props.navigate.state.voteId,
+            name: "",
+            content: "",
+            descriptio: "",
+            privacy	: "",
+            voteCount:	"",
+            blogUrl	: "", //发布者的blog
+            publisher: "",
+            publisherId: "",	
+            schoolClassId:	"",
+            deadline	: "",
+            dateAdded	: "",
+            isFinished: "",
         }
     }
+
     _isMounted;
-    componentWillMount = ()=>{
-        this._isMounted=true;
-		let contenturl = Config.BlogDetail+this.props.navigation.state.params.Id+'/body';
-        Service.Get(contenturl).then((jsonData)=>{
-            if(jsonData!=='rejected'){
+
+    componentWillMount = () => {
+        this._isMounted = true;
+        let contenturl = Config.VoteDetail + this.voteId + '/body';
+        Service.Get(contenturl).then((jsonData) => {
+            if (jsonData !== 'rejected') {
                 this.setState({
                     isRequestSuccess: true,
                 })
-                if(this._isMounted){
+                if (this._isMounted) {
                     this.setState({
                         content: jsonData,
                     })
                 }
             }
-        }).then(()=>{
-			global.storage.save({key:StorageKey.BLOGDETAIL+this.props.navigation.state.params.Id,data:this.state.content})
-			.catch((err)=>{
-				ToastAndroid.show("error",ToastAndroid.SHORT);
-			})
-		})
-		.catch((error) => {
-            ToastAndroid.show(err_info.NO_INTERNET,ToastAndroid.SHORT);
-			global.storage.load({key:StorageKey.BLOGDETAIL+this.props.navigation.state.params.Id})
-			.then((ret)=>{
-				this.setState({
-					content: ret,
-				})
-			})
-			.catch((err)=>{
-				ToastAndroid.show(err_info.TIME_OUT,ToastAndroid.SHORT)
-			})
-        });
+        }).then(() => {
+            global.storage.save({ key: StorageKey.BLOGDETAIL + this.props.navigation.state.params.Id, data: this.state.content })
+                .catch((err) => {
+                    ToastAndroid.show("error", ToastAndroid.SHORT);
+                })
+        })
+            .catch((error) => {
+                ToastAndroid.show(err_info.NO_INTERNET, ToastAndroid.SHORT);
+                global.storage.load({ key: StorageKey.BLOGDETAIL + this.props.navigation.state.params.Id })
+                    .then((ret) => {
+                        this.setState({
+                            content: ret,
+                        })
+                    })
+                    .catch((err) => {
+                        ToastAndroid.show(err_info.TIME_OUT, ToastAndroid.SHORT)
+                    })
+            });
     }
-    componentWillUnmount = ()=>{
-        this._isMounted=false;
+    componentWillUnmount = () => {
+        this._isMounted = false;
     }
 
     /** 评论的按钮的onPress */
-    _onPress = ()=>{
+    _onPress = () => {
         /* useURL为true表明参数Id不是博文Id而是博客Id，因API未返回博文Id，
            所以点击评论图标时屏蔽显示评论的功能，如果能够解决返回博文Id的问题，
            可以删掉第一个if语句。*/
@@ -83,24 +95,26 @@ export default class BlogDetail extends Component{
             Alert.alert('提示', 'API暂不支持，此功能敬请期待！');
             return;
         }
-        this.props.navigation.navigate('BlogComment',{
+        this.props.navigation.navigate('BlogComment', {
             blogApp: this.props.navigation.state.params.blogApp,
-            CommentCount: this.props.navigation.state.params.CommentCount+100,
+            CommentCount: this.props.navigation.state.params.CommentCount + 100,
             Id: this.props.navigation.state.params.Id,
         });
     }
-    _onPressEdit = ()=>{
-      if (global.user_information.BlogApp ===
-        this.props.navigation.state.params.blogApp)
-        {
-          this.props.navigation.navigate('BlogEdition',{
-            Id:this.props.navigation.state.params.Id,});
+    _onPressEdit = () => {
+        if (global.user_information.BlogApp ===
+            this.props.navigation.state.params.blogApp) {
+            this.props.navigation.navigate('BlogEdition', {
+                Id: this.props.navigation.state.params.Id,
+            });
         }
     }
 
-    _onPressBookmarks = ()=>{
-        this.props.navigation.navigate('BlogBookmarks',{Url: this.props.navigation.state.params.Url,
-            Title: this.props.navigation.state.params.Title});
+    _onPressBookmarks = () => {
+        this.props.navigation.navigate('BlogBookmarks', {
+            Url: this.props.navigation.state.params.Url,
+            Title: this.props.navigation.state.params.Title
+        });
     }
 
     /**选择博文内容来源。因为班级博文列表的API没有返回博文ID，只返回了URL。
@@ -114,7 +128,7 @@ export default class BlogDetail extends Component{
     _WebViewSourceParams() {
         let content = ContentHandler(this.state);
         if (this.props.navigation.state.params.useURL) {
-            return {uri: this.props.navigation.state.params.Url};
+            return { uri: this.props.navigation.state.params.Url };
         } else {
             return {
                 html: content,
@@ -123,56 +137,56 @@ export default class BlogDetail extends Component{
         }
     }
 
-    render(){
-        return(
-            <View style = {styles.container}>
+    render() {
+        return (
+            <View style={styles.container}>
                 <View
-                    style= {{
+                    style={{
                         alignSelf: 'stretch',
-                        flex:1,
+                        flex: 1,
                     }}
                 >
-                <WebView
-                    source={this._WebViewSourceParams()}
-                    style={{height: height-70}}
-                    startInLoadingState={true}
-                    domStorageEnabled={true}
-                    javaScriptEnabled={true}
-                    scalesPageToFit={true}
-                    onError = {()=>Alert.alert('网络异常，请稍后再试！')}
-                />
+                    <WebView
+                        source={this._WebViewSourceParams()}
+                        style={{ height: height - 70 }}
+                        startInLoadingState={true}
+                        domStorageEnabled={true}
+                        javaScriptEnabled={true}
+                        scalesPageToFit={true}
+                        onError={() => Alert.alert('网络异常，请稍后再试！')}
+                    />
                 </View>
-                <View style = {{height: 1, backgroundColor: 'rgb(204,204,204)', alignSelf:'stretch'}}/>
-                <View style = {styles.bottom}>
-                    <TouchableOpacity style = {styles.touchbutton} onPress = {this._onPress}>
-                        <Image source = {require('../images/comment.png')} style = {styles.imagestyle}
-                        accessibilityLabel = 'BlogDetail_commentImage'/>
+                <View style={{ height: 1, backgroundColor: 'rgb(204,204,204)', alignSelf: 'stretch' }} />
+                <View style={styles.bottom}>
+                    <TouchableOpacity style={styles.touchbutton} onPress={this._onPress}>
+                        <Image source={require('../images/comment.png')} style={styles.imagestyle}
+                            accessibilityLabel='BlogDetail_commentImage' />
 
                     </TouchableOpacity>
                     {
                         /* 无相关API支持，本功能暂时搁置 */
                         (global.user_information.BlogApp !==
-                        this.props.navigation.state.params.blogApp)
-                        ?(null): (null)
-                          /*
-                          <View>
-                            <TouchableOpacity style = {styles.touchbutton}
-                              onPress = {this._onPressEdit}>
-                                <Image source =
-                                {require('../images/editBlog.png')}
-                                style = {styles.imagestyle}
-                                accessibilityLabel = 'BlogDetail_editImage'/>
+                            this.props.navigation.state.params.blogApp)
+                            ? (null) : (null)
+                        /*
+                        <View>
+                          <TouchableOpacity style = {styles.touchbutton}
+                            onPress = {this._onPressEdit}>
+                              <Image source =
+                              {require('../images/editBlog.png')}
+                              style = {styles.imagestyle}
+                              accessibilityLabel = 'BlogDetail_editImage'/>
 
-                            </TouchableOpacity>
-                          </View>
-                           */
+                          </TouchableOpacity>
+                        </View>
+                         */
                     }
-                    <TouchableOpacity style = {styles.touchbutton}
-                      onPress = {this._onPressBookmarks}>
-                        <Image source =
-                        {require('../images/bookmarks.png')}
-                        style = {styles.imagestyle}
-                        accessibilityLabel = 'BlogDetail_markImage'/>
+                    <TouchableOpacity style={styles.touchbutton}
+                        onPress={this._onPressBookmarks}>
+                        <Image source=
+                            {require('../images/bookmarks.png')}
+                            style={styles.imagestyle}
+                            accessibilityLabel='BlogDetail_markImage' />
 
                     </TouchableOpacity>
                     <ShareButton
@@ -190,14 +204,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
-        flex:1,
+        flex: 1,
         alignSelf: 'stretch',
     },
     bottom: {
         flexDirection: 'row',
         justifyContent: 'flex-end',
         alignItems: 'center',
-        height: height/14,
+        height: height / 14,
         width: width,
         backgroundColor: 'white'
     },
@@ -205,12 +219,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
-        width: height/14,
-        height: height/14,
+        width: height / 14,
+        height: height / 14,
     },
     imagestyle: {
-        width: height/18,
-        height: height/22,
+        width: height / 18,
+        height: height / 22,
         resizeMode: 'stretch',
     }
 })

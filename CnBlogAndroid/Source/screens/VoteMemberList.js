250e7f48142681deffca000a43dde7de63d6302e
kilotron <cqycczq@126.com>
2019-05-05 22:35:55 +0800

    完善投票成员列表

--- CnBlogAndroid/Source/screens/VoteMemberList.js
+++ CnBlogAndroid/Source/screens/VoteMemberList.js
@@ -13,6 +13,11 @@ import {
 } from 'react-native';
 import * as Service from '../request/request.js';
 import Config from '../config';
+import {flatStyles} from '../styles/styles';
+import MyAdapter from './MyAdapter.js';
+
+const ItemHandler = require('../DataHandler/ClassMember/ItemHandler')
+
 
 export default class VoteMemberList extends Component {
     
@@ -20,7 +25,6 @@ export default class VoteMemberList extends Component {
         super(props);
         this.state = {
             members: [],
-            test: '111',
         }
     }
 
@@ -37,13 +41,6 @@ export default class VoteMemberList extends Component {
                     this.setState({
                         members: jsonData,
                     })
-                    s=''
-                    for (var i in jsonData) {
-                        s += 'displayname:' + jsonData[i].displayName+'\n'; 
-                        s += 'realname:' + jsonData[i].realName + '\n';
-                        s+='\n'
-                    }
-                    this.setState({test: s});
                 }
             }
         })
@@ -60,9 +57,75 @@ export default class VoteMemberList extends Component {
         this._isMounted = false;
     }
 
-    render() {
+    _renderItem = ({item}) => {
         return (
-            <View><Text>{this.state.test}</Text></View>
+            <View style={flatStyles.cell}>
+                <TouchableOpacity
+                    onPress={() => {alert('待完成：显示成员投票项')}}
+                    style={styles.listcontainer}
+                >
+                    <View style = {{flex:1}}>
+                        <Image 
+                            source = {
+                                item.avatarUrl?{uri:item.avatarUrl}:require('../images/defaultface.png')}
+                            style = {styles.avatarstyle}
+                        />
+                    </View>
+                    <View style = {styles.textcontainer}>
+                        <Text style = {{fontSize: 20,color: '#000000',flex:2}}>
+                        {item.displayName+ ItemHandler(item.realName)}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    render() {
+        var data=[];
+        for (var i in this.state.members) {
+            data.push({
+                memberId: this.state.members[i].memberId,
+                studentNo: this.state.members[i].studentNo, 
+                realName: this.state.members[i].realName,
+                displayName: this.state.members[i].displayName,
+                avatarUrl: this.state.members[i].avatarUrl,
+            })
+        }
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    renderItem={this._renderItem}
+                    data={data}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </View>
         )
     }
 }
+
+const screenWidth= MyAdapter.screenWidth;
+const screenHeight= MyAdapter.screenHeight;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    listcontainer: {
+        flexDirection: 'row',
+        justifyContent:'flex-start',
+        alignItems: 'flex-start',
+        flex:1,
+        backgroundColor: 'white',
+        marginLeft: 8,
+        marginRight: 12,
+        //alignSelf: 'stretch',
+    },
+    avatarstyle: {
+        width: 0.15*screenWidth,
+        height: 0.15*screenWidth,
+        marginBottom: 5,
+        marginTop: 5,
+        borderRadius : 40,
+        left : 2,
+    },
+})

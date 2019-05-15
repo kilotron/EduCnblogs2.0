1ede7d80974b34e9647f8b0cb2d0dcd52d545fc1
Welkin1027 <38560474+Welkin1027@users.noreply.github.com>
2019-05-15 20:35:34 +0800

    AddOption 和修改样式

--- CnBlogAndroid/Source/screens/VoteAdd.js
+++ CnBlogAndroid/Source/screens/VoteAdd.js
@@ -36,6 +36,9 @@ const abstractFontSize = MyAdapter.abstractFontSize;
 const informationFontSize = MyAdapter.informationFontSize;
 const btnFontSize = 16;
 const marginHorizontalNum = 16;
+const constBorderColor = 'gray';
+const constBorderDistance = screenHeight / 128;
+const constBorderMarginWidth = screenWidth / 32;
 
 export default class VoteAdd extends Component {
     constructor(props) {
@@ -82,13 +85,13 @@ export default class VoteAdd extends Component {
         var num, i;
         for (num = 0; num < varThisState.voteContents.length; num++) {
             if (this.judgeAddEmpty(varThisState.voteContents[num].title)) {
-                alert('问题' + (num+1) + '标题未填写');
+                alert('问题' + (num + 1) + '标题未填写');
                 return false;
             }
             var varVoteOptions = varThisState.voteContents[num].voteOptions;
             for (i = 0; i < varVoteOptions.length; i++) {
                 if (this.judgeAddEmpty(varVoteOptions[i].option)) {
-                    alert('问题' + (num+1) + ' 选项' + (i+1) + '未填写');
+                    alert('问题' + (num + 1) + ' 选项' + (i + 1) + '未填写');
                     return false;
                 }
             }
@@ -143,16 +146,24 @@ export default class VoteAdd extends Component {
     /** 投票名称*/
     getTitle() {
         return (
-            <View style={styles.title}>
-                <TextInput style={styles.bulletinDetail} multiline={true}
-                    placeholder={'请在此输入投票名称'}
-                    underlineColorAndroid="transparent"//设置下划线背景色透明 达到去掉下划线的效果
-                    onChangeText={(text) =>
-                        this.setState({ name: text })
-                    }
+            <View style={styles.titleAndContent}>
+                <View>
+                    <Text style={styles.text}>
+                        投票名称：
+                    </Text>
+                </View>
 
-                >
-                </TextInput>
+                <View style={styles.title}>
+                    <TextInput style={styles.bulletinDetail} multiline={true}
+                        placeholder={'请在此输入投票名称'}
+                        underlineColorAndroid="transparent"//设置下划线背景色透明 达到去掉下划线的效果
+                        onChangeText={(text) =>
+                            this.setState({ name: text })
+                        }
+
+                    >
+                    </TextInput>
+                </View>
             </View>
         );
     }
@@ -160,15 +171,24 @@ export default class VoteAdd extends Component {
     /** 投票说明 */
     getContent() {
         return (
-            <View style={styles.content}>
-                <TextInput style={styles.bulletinDetail} multiline={true}
-                    placeholder={'请在此输入投票说明'}
-                    underlineColorAndroid="transparent"//设置下划线背景色透明 达到去掉下划线的效果
-                    onChangeText={(text) =>
-                        this.setState({ content: text })
-                    }
-                ></TextInput>
+            <View style={styles.titleAndContent}>
+                <View>
+                    <Text style={styles.text}>
+                        投票详情：
+                    </Text>
+                </View>
+
+                <View style={styles.content}>
+                    <TextInput style={styles.bulletinDetail} multiline={true}
+                        placeholder={'请在此输入投票说明'}
+                        underlineColorAndroid="transparent"//设置下划线背景色透明 达到去掉下划线的效果
+                        onChangeText={(text) =>
+                            this.setState({ content: text })
+                        }
+                    ></TextInput>
+                </View>
             </View>
+
         );
     }
 
@@ -201,21 +221,27 @@ export default class VoteAdd extends Component {
     /** 投票隐私 */
     getPrivacy() {
         return (
-            <View style={styles.buttonContainer}>
-                <RadioModal
-                    selectedValue={this.state.privacy}
-                    onValueChange={(id, item) => this.setState({ privacy: id })}
-                    style={{
-                        flexDirection: 'row',
-                        flexWrap: 'wrap',
-                        alignItems: 'flex-start',
-                        flex: 1,
-                        backgroundColor: '#ffffff', padding: 5, marginTop: 10
-                    }}
-                >
-                    <Text value={1}>公开</Text>
-                    <Text value={2}>匿名</Text>
-                </RadioModal>
+            <View style={styles.titleAndContent}>
+                <View>
+                    <Text style={styles.text}>
+                        投票隐私：
+                    </Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <RadioModal
+                        selectedValue={this.state.privacy}
+                        onValueChange={(id, item) => this.setState({ privacy: id })}
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            flex: 1,
+                            backgroundColor: '#ffffff'
+                        }}
+                    >
+                        <Text value={1}>公开</Text>
+                        <Text value={2}>匿名</Text>
+                    </RadioModal>
+                </View>
             </View>
         );
     }
@@ -232,10 +258,9 @@ export default class VoteAdd extends Component {
 
     /** 获得整个投票内容 */
     getAllVoteContent() {
-        {/** 这个地方应该有投票添加按钮，每个投票都有唯一的标识符，是其在voteContent中的index */ }
-
+        /** 这个地方应该有投票添加按钮，每个投票都有唯一的标识符，是其在voteContent中的index */
         var array = [];
-        {/** 生成一个初始问题 */ }
+        /** 生成一个初始问题 */
         var num = 0;
         for (num = 1; num <= this.state.queationNum; num++) {
             if (num <= this.state.questionInitial) { //如果小于等于初始值，则不设定删除按钮
@@ -268,29 +293,43 @@ export default class VoteAdd extends Component {
         )
     }
 
+    /** 投票头部 */
+    getTitlePart() {
+        /** 欠缺添加问题按钮 */
+
+        return (
+            <View style={styles.titlePart}>
+                {/**投票名称 */}
+                {this.getTitle()}
+
+                {/**投票说明 */}
+                {this.getContent()}
+
+                {/**投票日期 */}
+                {this.getCalendar()}
+                <View style={styles.calendar}>
+                    <MyBar
+                        title="截止时间："
+                        onPress={() => { this.setState({ ModalVisible: true }); }}
+                        placeholder={this.state.deadline}
+                        myThis={this}
+                    />
+                </View>
+
+                {/** 投票隐私 */}
+                {this.getPrivacy()}
+
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <KeyboardAwareScrollView>
-                    {/**投票名称 */}
-                    {this.getTitle()}
 
-                    {/**投票说明 */}
-                    {this.getContent()}
-
-                    {/**投票日期 */}
-                    {this.getCalendar()}
-                    <View style={styles.container}>
-                        <MyBar
-                            title="投票截止时间"
-                            onPress={() => { this.setState({ ModalVisible: true }); }}
-                            placeholder={this.state.deadline}
-                            myThis={this}
-                        />
-                    </View>
-
-                    {/** 投票隐私 */}
-                    {this.getPrivacy()}
+                    {/** 投票头部 */}
+                    {this.getTitlePart()}
 
                     {/** 整个投票内容 */}
                     {this.getAllVoteContent()}
@@ -304,6 +343,22 @@ export default class VoteAdd extends Component {
 }
 
 const styles = StyleSheet.create({
+
+    titleAndContent: {
+        marginHorizontal: constBorderMarginWidth,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        
+    },
+
+    titlePart: { //投票头部
+
+    },
+
+    questionPart: { //问题部分
+
+    },
 
     optionContainer: { //与voteContentTitle完全相同
         flexDirection: 'row',
@@ -328,29 +383,37 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
+        alignSelf: 'center',
+        marginHorizontal: constBorderMarginWidth
+    },
+
+    calendar: {
+        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
         alignSelf: 'stretch',
-        marginTop: 16,
-        marginHorizontal: 16
+        //marginTop: constBorderDistance,
     },
 
     container: {
         backgroundColor: 'white',
         flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'stretch',
-        // alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        alignSelf: 'stretch',
     },
     text: {
-        width: 0.2 * screenWidth,
+        width: 0.25 * screenWidth,
         fontSize: 16,
         color: 'black',
         textAlign: 'left',
     },
     textInput: {
         flex: 1,
-        marginLeft: 8,
+        //marginLeft: 8,
         height: screenHeight / 16,
-        borderColor: 'gray',
+        borderColor: constBorderColor,
         borderWidth: 1
     },
     picker: {
@@ -371,15 +434,19 @@ const styles = StyleSheet.create({
         paddingTop: 40
     },
     content: {
+        marginTop: constBorderDistance,
         height: screenHeight / 8,
-        borderColor: UI.TOP_COLOR,
+        borderColor: constBorderColor,
         borderWidth: 1,
+        flex: 1,
     },
     title: {
-        //flex : 1,
+        marginTop: constBorderDistance,
         height: screenHeight / 16,
-        borderColor: UI.TOP_COLOR,
+        borderColor: constBorderColor,
         borderWidth: 1,
+        flex: 1,
+        //width
     },
     commitBtn: {
         flex: 1,
@@ -399,10 +466,12 @@ const styles = StyleSheet.create({
         width: screenWidth / 4,
     },
     buttonContainer: {
+        flex : 1,
+        //marginTop: constBorderDistance,
         justifyContent: 'center',
         flexDirection: 'row',
         height: screenHeight / 16,
-        borderColor: UI.TOP_COLOR,
+        borderColor: 'transparent',
         borderWidth: 1,
     },
 });
@@ -541,7 +610,7 @@ class Question extends Component {
             voteOptionsText: [], //放文本的
             voteTitle: '',
             voteMode: 1, //初始设置成1，即单选
-            picture : null,
+            picture: null,
             questionEntire: [], //整个问题的主体部分
         }
     }
@@ -602,19 +671,13 @@ class Question extends Component {
         if (nextState.optionNum > array.length) { //证明要添加新的
             varOptionRank++;
             array.push(
-                {
-                    'optionObject'
-                        :
-                        <Option
-                            deleteButton={true} //需要有删除按钮
-                            titleNum={varOptionRank}
-                            optionInitial={this.state.optionInitial}
-                            myThis={this}
-                            isVisible={true}
-                        />,
-                    'titleNum': { num },
-                    'text': '',
-                }
+                <Option
+                    deleteButton={false} //这里决定删除按钮
+                    titleNum={varOptionRank}
+                    optionInitial={this.state.optionInitial}
+                    myThis={this}
+                    isVisible={true}
+                />
             );
         }
         this.setState({ optionRank: varOptionRank });
@@ -717,12 +780,11 @@ class Question extends Component {
                     问题{this.state.titleNum}
                 </Text>
 
-                {/*暂时不上线这个功能*/}
                 {/* 添加选项*/}
-                {/* <Button
+                <Button
                     onPress={() => { this._onpress2AddOption() }}
                     title='添加选项'
-                /> */}
+                />
 
                 {/** 标题部分 */}
                 {this.getVoteContentTitle()}

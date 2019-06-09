0abe52c8ac2d48cb65f27766c8ee85035c34151f
Welkin1027 <38560474+Welkin1027@users.noreply.github.com>
2019-06-09 17:00:30 +0800

    添加成员页面适配

--- CnBlogAndroid/Source/screens/ClassMember.js
+++ CnBlogAndroid/Source/screens/ClassMember.js
@@ -1,13 +1,13 @@
 import Config from '../config';
 import api from '../api/api.js';
-import {authData} from '../config'
+import { authData } from '../config'
 import * as Service from '../request/request.js'
 import MyAdapter from './MyAdapter.js';
-import React, { Component} from 'react';
-import {StorageKey} from '../config'
-import {err_info} from '../config'
+import React, { Component } from 'react';
+import { StorageKey } from '../config'
+import { err_info } from '../config'
 import { Container, Header, Fab, Button, Icon } from 'native-base';
-import {flatStyles} from '../styles/styles';
+import { flatStyles } from '../styles/styles';
 
 import {
     StyleSheet,
@@ -26,15 +26,15 @@ import {
     TabNavigator,
 } from 'react-navigation';
 import { getHeaderStyle } from '../styles/theme-context';
-const screenWidth= MyAdapter.screenWidth;
-const screenHeight= MyAdapter.screenHeight;
-const titleFontSize= MyAdapter.titleFontSize;
-const abstractFontSize= MyAdapter.abstractFontSize;
-const informationFontSize= MyAdapter.informationFontSize;
-const btnFontSize= MyAdapter.btnFontSize;   
+const screenWidth = MyAdapter.screenWidth;
+const screenHeight = MyAdapter.screenHeight;
+const titleFontSize = MyAdapter.titleFontSize;
+const abstractFontSize = MyAdapter.abstractFontSize;
+const informationFontSize = MyAdapter.informationFontSize;
+const btnFontSize = MyAdapter.btnFontSize;
 const ItemHandler = require('../DataHandler/ClassMember/ItemHandler')
 
-export default class ClassMember extends Component{
+export default class ClassMember extends Component {
 
     static navigationOptions = ({ navigation }) => ({
         /* 使用global.theme的地方需要单独在页面写static navigationOptions,
@@ -43,7 +43,7 @@ export default class ClassMember extends Component{
         headerTintColor: global.theme.headerTintColor,
     })
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             members: [],
@@ -53,133 +53,110 @@ export default class ClassMember extends Component{
     }
     _isMounted;
 
-    componentWillMount = ()=>{
-        this._isMounted=true;
+    componentWillMount = () => {
+        this._isMounted = true;
         let url = Config.MemberList + this.props.navigation.state.params.classId;
-        Service.Get(url).then((jsonData)=>{
-            if(jsonData!=='rejected')
-            {
+        Service.Get(url).then((jsonData) => {
+            if (jsonData !== 'rejected') {
                 this.setState({
                     isRequestSuccess: true,
                 })
-                if(this._isMounted){
+                if (this._isMounted) {
                     this.setState({
                         members: jsonData,
                     })
                 }
             }
         })
-        .then(()=>{
-            global.storage.save({key:StorageKey.CLASS_MEMBER , data:this.state.members});
-        })
-        .catch((error) => {
-            ToastAndroid.show(err_info.NO_INTERNET,ToastAndroid.SHORT);
-            global.storage.load({key:StorageKey.CLASS_MEMBER})
-            .then((ret)=>{
-                this.setState({
-                    members : ret,
-                })
-            }).catch((err)=>{
-                ToastAndroid.show(err_info.TIME_OUT,ToastAndroid.SHORT);
-                this.props.navigation.navigate('Loginer');
+            .then(() => {
+                global.storage.save({ key: StorageKey.CLASS_MEMBER, data: this.state.members });
             })
-        });
-		
+            .catch((error) => {
+                ToastAndroid.show(err_info.NO_INTERNET, ToastAndroid.SHORT);
+                global.storage.load({ key: StorageKey.CLASS_MEMBER })
+                    .then((ret) => {
+                        this.setState({
+                            members: ret,
+                        })
+                    }).catch((err) => {
+                        ToastAndroid.show(err_info.TIME_OUT, ToastAndroid.SHORT);
+                        this.props.navigation.navigate('Loginer');
+                    })
+            });
+
         let url1 = Config.apiDomain + api.user.info;
-        Service.Get(url1).then((jsonData)=>{
-            let url2= Config.apiDomain+"api/edu/member/"+jsonData.BlogId+"/"+this.props.navigation.state.params.classId;
-            Service.Get(url2).then((jsonData)=>{
-                if(this._isMounted){
+        Service.Get(url1).then((jsonData) => {
+            let url2 = Config.apiDomain + "api/edu/member/" + jsonData.BlogId + "/" + this.props.navigation.state.params.classId;
+            Service.Get(url2).then((jsonData) => {
+                if (this._isMounted) {
                     this.setState({
                         membership: jsonData.membership,
                     })
                 }
             })
         }).catch((error) => {
-            ToastAndroid.show(err_info.NO_INTERNET,ToastAndroid.SHORT);
+            ToastAndroid.show(err_info.NO_INTERNET, ToastAndroid.SHORT);
         });
     }
-    UpdateData = ()=>{
+    UpdateData = () => {
         this.setState({
             isRequestSuccess: false,
         })
         this.componentWillMount();
     }
-    componentWillUnmount=()=>{
-        this._isMounted=false;
+    componentWillUnmount = () => {
+        this._isMounted = false;
     }
-    _renderItem = (item)=>{
+    _renderItem = (item) => {
         let item1 = item;
-        let {blogUrl,displayName,avatarUrl,membership,realName,blogId} = item1.item;
-        return(
+        let { blogUrl, displayName, avatarUrl, membership, realName, blogId } = item1.item;
+        return (
             <View style={flatStyles.cell}>
                 <TouchableOpacity
-                    onPress = {()=>this.props.navigation.navigate('MemberBlog',{blogId:blogId,blogUrl: blogUrl})}
-                    style = {[styles.listcontainer, {backgroundColor:global.theme.backgroundColor}]}
+                    onPress={() => this.props.navigation.navigate('MemberBlog', { blogId: blogId, blogUrl: blogUrl })}
+                    style={[styles.listcontainer, { backgroundColor: global.theme.backgroundColor }]}
                 >
-                    <View style = {{flex:1}}>
-                        <Image source = {avatarUrl?{uri:avatarUrl}:require('../images/defaultface.png')} style = {[styles.avatarstyle, {opacity: global.theme.imageOpacity}]}/>
+                    <View style={{ flex: 1 }}>
+                        <Image source={avatarUrl ? { uri: avatarUrl } : require('../images/defaultface.png')} style={[styles.avatarstyle, { opacity: global.theme.imageOpacity }]} />
                     </View>
-                    <View style = {[styles.textcontainer, {backgroundColor:global.theme.backgroundColor}]}>
-                        <Text style = {{fontSize: 20,color: global.theme.textColor,flex:2}}>{displayName+ ItemHandler(realName)}</Text>
-                        <Text style = {{fontSize: 15,color: global.theme.grayTextColor,flex:3}}>{membership===1?'学生':membership===2?'老师':'助教'}</Text>
+                    <View style={[styles.textcontainer, { backgroundColor: global.theme.backgroundColor }]}>
+                        <Text style={{ fontSize: 20, color: global.theme.textColor, flex: 2 }}>{displayName + ItemHandler(realName)}</Text>
+                        <Text style={{ fontSize: 15, color: global.theme.grayTextColor, flex: 3 }}>{membership === 1 ? '学生' : membership === 2 ? '老师' : '助教'}</Text>
                     </View>
                 </TouchableOpacity>
             </View>
         )
     }
 
-    _onPress = ()=>{
-        if(this.state.membership!=2 && this.state.membership!=3)
-        {
-            ToastAndroid.show("您没有权限，只有老师和助教才能添加班级成员哦！",ToastAndroid.SHORT);
+    _onPress = () => {
+        if (this.state.membership != 2 && this.state.membership != 3) {
+            ToastAndroid.show("您没有权限，只有老师和助教才能添加班级成员哦！", ToastAndroid.SHORT);
         }
-        else
-        {
+        else {
             this.props.navigation.navigate('ClassMemberAdd',
-            {classId: this.props.navigation.state.params.classId})
+                { classId: this.props.navigation.state.params.classId })
         }
     }
-    generateAddButton(){
-        if(this.state.membership == 2 || this.state.membership == 3){
-            return(
-                <TouchableHighlight 
-                    underlayColor="#3b50ce"
+    generateAddButton() {
+        if (this.state.membership == 2 || this.state.membership == 3) {
+            return (
+                <TouchableHighlight
+                    underlayColor={global.theme.addUnderlayColor}
                     activeOpacity={0.5}
-                    style={{
-                        position:'absolute',
-                        bottom:20,
-                        right:10, 
-                        backgroundColor: "#3b50ce",
-                        width: 52, 
-                        height: 52, 
-                        borderRadius: 26,
-                        justifyContent: 'center', 
-                        alignItems: 'center', 
-                        margin: 20}} 
-                        onPress={this._onPress} >
-                    
-                    <Text
-                        style= {{
-                            fontSize: 30,
-                            color: '#ffffff',
-                            textAlign: 'center',
-                            fontWeight: '100',
-                        }}
-                    >
+                    style={[styles.addButton, { backgroundColor: global.theme.addBackgroundColor }]}
+                    onPress={this._onPress.bind(this)} >
+                    <Text style={[styles.buttonContent, { color: global.theme.addTextColor }]}>
                         +
                     </Text>
-                    
                 </TouchableHighlight>
             );
         }
         else return;
     }
-    render(){
+    render() {
         var data = [];
         // 在请求成功的情况下渲染列表
-        for(var i in this.state.members)
-        {
+        for (var i in this.state.members) {
             data.push({
                 key: this.state.members[i].memberId,//成员Id
                 blogUrl: this.state.members[i].blogUrl,//博客地址
@@ -191,26 +168,26 @@ export default class ClassMember extends Component{
             })
         }
 
-        return(
-            <View style = {[styles.container, {backgroundColor:global.theme.backgroundColor}]}>
-				
-                <View style={{ height: 1, backgroundColor: global.theme.backgroundColor,  marginTop: 0.005*screenHeight, alignSelf:'stretch'}}/>
+        return (
+            <View style={[styles.container, { backgroundColor: global.theme.backgroundColor }]}>
+
+                <View style={{ height: 1, backgroundColor: global.theme.backgroundColor, marginTop: 0.005 * screenHeight, alignSelf: 'stretch' }} />
                 <View
-                    style= {{
+                    style={{
                         flexDirection: 'row',
-                        justifyContent:'flex-start',
+                        justifyContent: 'flex-start',
                         alignItems: 'flex-start',
                         alignSelf: 'stretch',
-                        flex:1,
+                        flex: 1,
                     }}
                 >
                     <FlatList
                         renderItem={this._renderItem}
                         data={data}
-                        onRefresh = {this.UpdateData}
-                        refreshing= {false}
+                        onRefresh={this.UpdateData}
+                        refreshing={false}
                     />
-					{this.generateAddButton()}
+                    {this.generateAddButton()}
                 </View>
             </View>
         )
@@ -226,27 +203,46 @@ const styles = StyleSheet.create({
     },
     listcontainer: {
         flexDirection: 'row',
-        justifyContent:'flex-start',
+        justifyContent: 'flex-start',
         alignItems: 'flex-start',
-        flex:1,
+        flex: 1,
         backgroundColor: 'white',
         marginLeft: 8,
         marginRight: 12,
         //alignSelf: 'stretch',
     },
     avatarstyle: {
-        width: 0.15*screenWidth,
-        height: 0.15*screenWidth,
+        width: 0.15 * screenWidth,
+        height: 0.15 * screenWidth,
         marginBottom: 5,
         marginTop: 5,
-        borderRadius : 40,
-        left : 2,
+        borderRadius: 40,
+        left: 2,
     },
     textcontainer: {
-        justifyContent:'flex-start',
+        justifyContent: 'flex-start',
         alignItems: 'flex-start',
         flex: 4,
         backgroundColor: 'white',
         //alignSelf: 'stretch',
-    }
+    },
+    addButton: {
+        position: 'absolute',
+        bottom: 20,
+        right: 10,
+        backgroundColor: "#3b50ce",
+        width: 52,
+        height: 52,
+        borderRadius: 26,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20
+    },
+    buttonContent: { //button 内的字体
+        fontSize: 30,
+        color: '#ffffff',
+        textAlign: 'center',
+        fontWeight: '100',
+    },
+
 });
